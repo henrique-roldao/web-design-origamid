7bd1e1d403b30f90e3db81117c8baef60fc3dd33
henrique-roldao <henriqueroldao@rede.ulbra.br>
2020-12-15 01:06:39 -0300

    :tada: Javascript adicionado!

---
+++ web/js/script.js
@@ -0,0 +1,19 @@
+if(window.SimpleSlide) {
+
+    new SimpleSlide({
+        slide: "quote",
+        time: 5000
+    });
+    
+    new SimpleSlide({
+        slide: "portfolio",
+        time: 5000,
+        nav: true
+    });
+
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}
+
